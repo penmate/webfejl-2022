9654378375cc9d7736a07c58465c268f11f5cbca
Mate Pejnovac <85074903+penmate@users.noreply.github.com>
2022-06-02 20:11:01 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   firebase: {
-    projectId: 'webfelj-2022',
-    appId: '1:283641505813:web:88f7631af5b1d50e10861c',
-    storageBucket: 'webfelj-2022.appspot.com',
-    apiKey: '',
-    authDomain: 'webfelj-2022.firebaseapp.com',
-    messagingSenderId: '283641505813',
-    measurementId: 'G-ZB92Y7VS7Y',
+/* TODO: put your environment here */
   },
   production: true
 };
