c1e967da45da45eec30f009c3e51027c51428581
Mate Pejnovac <85074903+penmate@users.noreply.github.com>
2022-06-02 19:54:28 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     projectId: 'webfelj-2022',
     appId: '1:283641505813:web:88f7631af5b1d50e10861c',
     storageBucket: 'webfelj-2022.appspot.com',
-    apiKey: 'AIzaSyCQcgpArQ_-QXIwjlox_lJgYsXX7aEUuqY',
+    apiKey: '',
     authDomain: 'webfelj-2022.firebaseapp.com',
     messagingSenderId: '283641505813',
     measurementId: 'G-ZB92Y7VS7Y',
